a9ca4063dd2be82e609e07dcbdbedf6695643dc2
Giovanni Pereira <80793459+giovannipereiradev@users.noreply.github.com>
2023-08-23 16:15:40 -0300

    404 Page + Finish 991px

--- javaScript/animation.js
+++ javaScript/animation.js
@@ -2,10 +2,18 @@ function clickMenu() {
     if (nav.style.display == 'flex') {
         nav.style.display = 'none'
         body.style.overflow = 'visible'
+        main.style.filter = 'blur(0px)'
     } else {
         nav.style.display = 'flex'
         body.style.overflow = 'hidden'
+        main.style.filter = 'blur(3px)'
     }
+}
+
+function closeMenu() {
+    nav.style.display = 'none'
+    body.style.overflow = 'visible'
+    main.style.filter = 'blur(0px)'
 }
 
 function clickMenuTranslate() {
